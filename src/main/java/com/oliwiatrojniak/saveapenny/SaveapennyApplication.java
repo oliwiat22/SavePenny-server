d55e1ae9b5539381f342bda887bbaab1c3b09c3c
oliwiat22 <oliwiatrojniak@gmail.com>
2024-01-04 22:37:29 +0100

    Initial commit

---
+++ src/main/java/com/oliwiatrojniak/saveapenny/SaveapennyApplication.java
@@ -0,0 +1,13 @@
+package com.oliwiatrojniak.saveapenny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveapennyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveapennyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oliwiatrojniak/saveapenny/SaveapennyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oliwiatrojniak.saveapenny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveapennyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
